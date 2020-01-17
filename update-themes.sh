4c26523fd9200664ebe34ba9808aa969e7278750
zxz <zxz@zxzdeMacBook-Pro.local>
2020-01-17 17:44:04 +0800

    修复更新脚本

--- update-themes.sh
+++ update-themes.sh
@@ -1,17 +1,23 @@
 #! /bin/bash
 
 # 自动更新主题
+theme=next
+themeAddress="https://github.com/theme-next/hexo-theme-next"
+oldTheme=$theme-old
+
+cd themes
 
 echo  将原来的主题文件夹改名
-mv themes/next themes/next-old
+mv next $oldTheme
 
 echo 下载最新的主题
-git clone https://github.com/theme-next/hexo-theme-next themes/next
+git clone $themeAddress $theme
 
 echo 将原来的主题文件覆盖到新的主题
-mv themes/next-old/_config.yml themes/next/_config.yml
+mv $oldTheme/_config.yml $theme/_config.yml
 
 echo 删除老的主题
-rm -rf themes/next-old
+rm -rf $oldTheme
 
-rm -rf themes/next/.git
+echo 删除.git
+rm -rf $theme/.git
